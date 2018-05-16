8cc5a279b0e453076078c2fb59a8d87f1246726b
Andrei Terziev <andrei.terziev@egt-bg.com>
2018-05-16 18:06:26 +0300

    update source

--- table/app/src/main/java/com/example/adefault/table/addSuma.java
+++ table/app/src/main/java/com/example/adefault/table/addSuma.java
@@ -3,11 +3,14 @@ package com.example.adefault.table;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,8 @@ import java.util.Calendar;
 public class addSuma extends AppCompatActivity {
 
     ListView listView;
+    Button open;
+    TextView cost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,27 @@ public class addSuma extends AppCompatActivity {
                 "List View Array Adapter",
                 "Android Example List View"
         };
+
+        open = (Button) findViewById(R.id.open);
+        cost = (TextView) findViewById(R.id.etCost);
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), open);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getApplicationContext(), "" + item.getTitle(), Toast.LENGTH_LONG).show();
+                        cost.setText("Разход за " + item.getTitle());
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
 
 
         Calendar calendar = Calendar.getInstance();
