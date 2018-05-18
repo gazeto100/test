32263f2f15125276510530e374802808d27917df
Andrei Terziev <andrei.terziev@egt-bg.com>
2018-05-18 18:00:03 +0300

    add new cource

--- table/app/src/main/java/com/example/adefault/table/accounts.java
+++ table/app/src/main/java/com/example/adefault/table/accounts.java
@@ -1,5 +1,9 @@
 package com.example.adefault.table;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
@@ -7,6 +11,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -23,6 +28,54 @@ public class accounts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+    }
+
+
+    public void GetRow(final int id, final String Data, final String Suma, final String What){
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final CharSequence colors[] = new CharSequence[] {"Редактиране", "Изтриване"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(accounts.this);
+
+                builder.setTitle("Запис: " + Data + " " + Suma + " " + What);
+
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                        switch (which){
+                            case 0:
+                                break;
+
+                            case 1:
+                                String testString = Integer.toString(id);
+                                Integer deleteRow = myDb.deleteData(testString);
+
+                                Intent intent = getIntent();
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                finish();
+                                startActivity(intent);
+
+
+                                break;
+                            default:
+                                break;
+                        }
+
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+
+
+    public void showTable() {
         myDb = new Databasehepler(this);
 
 
@@ -54,47 +107,47 @@ public class accounts extends AppCompatActivity {
         db.beginTransaction();
         try
         {
-        String selectQuery = "SELECT * FROM "+ myDb.TABLE_NAME;
-        cursor = db.rawQuery(selectQuery,null);
+            String selectQuery = "SELECT * FROM "+ myDb.TABLE_NAME;
+            cursor = db.rawQuery(selectQuery,null);
 
-        if(cursor.getCount() >0)
-        {
-            int count = 0;
-            while (cursor.moveToNext()) {
+            if(cursor.getCount() >0)
+            {
+                int count = 0;
+                while (cursor.moveToNext()) {
 
-                int id = cursor.getInt(cursor.getColumnIndex("ID"));
-                String data= cursor.getString(cursor.getColumnIndex("DATA"));
-                String suma= cursor.getString(cursor.getColumnIndex("SUMA"));
-                String what= cursor.getString(cursor.getColumnIndex("WHAT"));
-                //String other= cursor.getString(cursor.getColumnIndex("OTHER"));
+                    int id = cursor.getInt(cursor.getColumnIndex("ID"));
+                    String data= cursor.getString(cursor.getColumnIndex("DATA"));
+                    String suma= cursor.getString(cursor.getColumnIndex("SUMA"));
+                    String what= cursor.getString(cursor.getColumnIndex("WHAT"));
+                    //String other= cursor.getString(cursor.getColumnIndex("OTHER"));
 
-                row = new TableRow(this);
+                    row = new TableRow(this);
 
-                if (count %2 == 0){
-                    row.setBackgroundColor(Color.WHITE);
-                }else {
+                    if (count %2 == 0){
+                        row.setBackgroundColor(Color.WHITE);
+                    }else {
 
-                }
+                    }
 
 //                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
 //                        TableLayout.LayoutParams.MATCH_PARENT));
-                String[] colText={data,suma,what};
-                for(String text:colText) {
-                    TextView tv = new TextView(this);
+                    String[] colText={data,suma,what};
+                    for(String text:colText) {
+                        TextView tv = new TextView(this);
 //                    tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
 //                            TableRow.LayoutParams.MATCH_PARENT));
 //                    tv.setGravity(Gravity.CENTER);
-                    tv.setTextSize(14);
-                    tv.setPadding(5, 10, 5, 5);
-                    tv.setText(text);
-                    row.addView(tv);
+                        tv.setTextSize(14);
+                        tv.setPadding(5, 10, 5, 5);
+                        tv.setText(text);
+                        row.addView(tv);
+                    }
+                    showTable.addView(row);
+                    GetRow(id, data, suma, what);
+                    count++;
                 }
-                showTable.addView(row);
-                GetRow(id);
-                count++;
             }
-        }
-        db.setTransactionSuccessful();
+            db.setTransactionSuccessful();
         }
         catch (SQLiteException e)
         {
@@ -108,20 +161,7 @@ public class accounts extends AppCompatActivity {
             db.close();
             // Close database
         }
-    }
 
 
-    public void GetRow(final int id){
-        row.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.setBackgroundColor(Color.GRAY);
-                String testString = Integer.toString(id);
-                Integer deleteRow = myDb.deleteData(testString);
-                //myDb.onUpgrade(myDb, 1, 1);
-
-                Toast.makeText(accounts.this, "Row clicked: " + v.getId(), Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
